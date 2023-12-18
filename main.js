c055d74eeca4b0b5f58d9ab330911bf984ef9213
Szymon Burak <sajmon0031@gmail.com>
2023-12-18 22:23:49 +0100

    Add mobile version

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ const showMorePortfolio = () => {
     portfolioAll.style.display = 'block';
     portfolioAll.style.visibility = 'visible';
 }
+
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
+
+const openMenu = () => {
+    mobileMenu.style.transform = 'scaleX(1)';
+    setTimeout(() => {
+        mobileMenuChildren.forEach((item) => {
+            item.style.opacity = '1';
+        });
+    }, 200);
+}
+
+const closeMenu = () => {
+    mobileMenuChildren.forEach((item) => {
+        item.style.opacity = '0';
+    });
+
+    setTimeout(() => {
+        mobileMenu.style.transform = 'scaleX(0)';
+    }, 200);
+}
